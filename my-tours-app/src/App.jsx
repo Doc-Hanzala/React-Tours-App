ddc0929125cb64b2e0749c1077fb232c1838fe81
hanzala tanveer <810hanzalatanveer@gmail.com>
2023-04-20 11:19:32 +0100

    added functionality

--- my-tours-app/src/App.jsx
+++ my-tours-app/src/App.jsx
@@ -1,8 +1,11 @@
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import Loading from "./components/Loading";
 import Tours from "./components/Tours";
 
 const url = "https://course-api.com/react-tours-project";
+
+export const DelToursContext = createContext();
+
 function App() {
   const [tours, setTours] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -12,33 +15,51 @@ function App() {
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setTours(data)
+      setTours(data);
     } catch (error) {
       console.log(error);
     }
-    setLoading(false)
+    setLoading(false);
+  };
+
+  const delTours = (id) => {
+    const updatedTours = tours.filter((tour) => tour.id !== id);
+    setTours(updatedTours);
   };
 
   useEffect(() => {
     getTours();
   }, []);
 
-  if(loading){
+  if (loading) {
     return (
       <main>
         <Loading />
       </main>
-    )
-     
-    
-  
+    );
   }
 
- return(
-  <main>
-    <Tours tours={tours} />
-  </main>
- )
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h3>No Tours Left</h3>
+          <div className="title-underline"></div>
+        </div>
+        <button onClick={getTours} className="btn">
+          Refresh
+        </button>
+      </main>
+    );
+  }
+
+  return (
+    <DelToursContext.Provider value={{ delTours }}>
+      <main>
+        <Tours tours={tours} />
+      </main>
+    </DelToursContext.Provider>
+  );
 }
 
 export default App;
--- my-tours-app/src/components/SingleTour.jsx
+++ my-tours-app/src/components/SingleTour.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { DelToursContext } from "../App";
 
-const SingleTour = ({id,name,info,image,price}) => {
+const SingleTour = ({ id, name, info, image, price }) => {
+  const { delTours } = useContext(DelToursContext);
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <section className="single-tour">
-        <img src={image} alt={name} />
-        <span className='price' >${price}</span>
-        <div className="tour-info">
-            <h4>{name}</h4>
-            <p>{info}</p>
-        <button className="btn">not interested</button>
-        </div>
+      <img src={image} alt={name} />
+      <span className="price">${price}</span>
+      <div className="tour-info">
+        <h4>{name}</h4>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button className="info-btn" onClick={() => setReadMore(!readMore)}>
+            {readMore ? "read less" : "read more"}
+          </button>
+        </p>
+        <button onClick={() => delTours(id)} className="btn">
+          not interested
+        </button>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default SingleTour
+export default SingleTour;
